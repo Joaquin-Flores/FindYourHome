e36163c840ed1c4cacfa2c93e5f3b42eefbd61d6
TrynowPht <pieroht1998@outlook.com>
2020-05-19 02:57:50 -0500

    Añade Clase tipo-publicador y publicar-inmueble. Ajustes en routing y app.html

---
+++ Frontend/src/app/components/tipo-publicador/tipo-publicador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-publicador',
+  templateUrl: './tipo-publicador.component.html',
+  styleUrls: ['./tipo-publicador.component.css']
+})
+export class TipoPublicadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
